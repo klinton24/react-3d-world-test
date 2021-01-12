b010e33581be2440d3b4b02a01effce08528e9b3
K Smith <klinton.jp@gmail.com>
2021-01-12 10:41:38 -0700

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas }  from "react-three-fiber";
+import { OrbitalControls } from "drei";
 
-function App() {
+function Box() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
   );
 }
-
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+      <OrbitalControls />
+      <ambientLight intensity={0.5} />
+      <Box />
+    </Canvas>
+  );
+}
