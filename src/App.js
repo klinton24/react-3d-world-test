f7a7c12facf31508d425171bd39e0fc3e1927c6b
unknown <klinton.jp@gmail.com>
2021-02-20 23:18:17 -0700

    Cleaned up files, added plane and physics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Canvas }  from "react-three-fiber";
-import { OrbitControls } from "drei";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Stars } from "drei";
+import { Physics, useBox, usePlane } from "use-cannon";
+import "./styles.css";
 
-function Box() {
+function Box(props) {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 10, 0] }));
   return (
-    <mesh>
+    <mesh onClick={() => {
+      api.velocity.set(0, 15, 0);
+    }} ref={ref} position={[0, 10, 0]}>
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI/2, 0, 0] }));
+  return (
+    <mesh rotation={[-Math.PI/2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100,100]} />
+      <meshLambertMaterial attach="material" color="darkgreen" />
     </mesh>
   );
 }
@@ -14,8 +29,13 @@ export default function App() {
   return (
     <Canvas>
       <OrbitControls />
+      <Stars />
       <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
       <Box />
+      <Plane />
+      </Physics>
     </Canvas>
   );
 }
